097b42d6f75d6305ad5bb6f33163887a3389ac27
Bradley-Bird <Bradleybird2@icloud.com>
2024-03-11 02:21:49 -0700

    changes zoom for mobile

--- src/components/VisMap.jsx
+++ src/components/VisMap.jsx
@@ -12,7 +12,7 @@ export default function VisMap() {
     return (
         <APIProvider apiKey={key}>
             <div style={{ height: '50vh', width: '100%' }}>
-                <Map zoom={18} center={position}>
+                <Map zoom={20} center={position}>
                     <Marker
                         // title={''} might not be needed
                         position={position}
