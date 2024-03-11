8238cfb8e923c58dbf3013f2787017c1387d4b1d
Bradley-Bird <Bradleybird2@icloud.com>
2024-03-11 02:20:59 -0700

    adds functionality to onclick for maps

--- src/components/VisMap.jsx
+++ src/components/VisMap.jsx
@@ -4,6 +4,11 @@ export default function VisMap() {
     const position = { lat: 45.99279, lng: -123.92522 };
     const key = import.meta.env.VITE_GOOGLE_API_KEY;
 
+    const openGoogleMaps = () => {
+        const url = `https://www.google.com/maps/search/?api=1&query=${position.lat},${position.lng}`;
+        window.open(url, '_blank');
+    };
+
     return (
         <APIProvider apiKey={key}>
             <div style={{ height: '50vh', width: '100%' }}>
@@ -11,6 +16,7 @@ export default function VisMap() {
                     <Marker
                         // title={''} might not be needed
                         position={position}
+                        onClick={openGoogleMaps}
                     ></Marker>
                 </Map>
             </div>
