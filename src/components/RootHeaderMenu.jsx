7a749939becd227d14efea6d0753b4ccaa3d06a3
bradley bird <bradleyb1rd2@icloud.com>
2024-08-06 02:13:56 -0700

    fixes problem where links werent clicking properly

--- src/components/RootHeaderMenu.jsx
+++ src/components/RootHeaderMenu.jsx
@@ -16,86 +16,106 @@ function RootHeaderMenu() {
                             <MenuIcon />
                         </Button>
                         <Menu {...bindMenu(popupState)}>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.main' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/">Home</StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.alt' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/beers">
-                                        Beers
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{
-                                    bgcolor: 'primary.main',
-                                }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/cocktails">
-                                        Cocktails
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.alt' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/na">
-                                        Non Alcoholic
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.main' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/appetizers">
-                                        Appetizers
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.alt' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/happy_hour">
-                                        Happy Hour
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.main' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/contact">
-                                        Contact
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem
-                                sx={{ bgcolor: 'primary.alt' }}
-                                onClick={popupState.close}
-                            >
-                                <Typography>
-                                    <StyledMenuLink to="/about">
-                                        About
-                                    </StyledMenuLink>
-                                </Typography>
-                            </MenuItem>
+                            <StyledMenuLink to="/">
+                                <MenuItem
+                                    sx={{
+                                        bgcolor: 'primary.main',
+                                    }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography align="right">
+                                        <StyledMenuLink to="/">
+                                            Home
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/beers">
+                                <MenuItem
+                                    sx={{ bgcolor: 'primary.alt' }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/beers">
+                                            Beers
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/cocktails">
+                                <MenuItem
+                                    sx={{
+                                        bgcolor: 'primary.main',
+                                    }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/cocktails">
+                                            Cocktails
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/na">
+                                <MenuItem
+                                    sx={{ bgcolor: 'primary.alt' }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/na">
+                                            Non Alcoholic
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/appetizers">
+                                <MenuItem
+                                    sx={{ bgcolor: 'primary.main' }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/appetizers">
+                                            Appetizers
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/happy_hour">
+                                <MenuItem
+                                    sx={{ bgcolor: 'primary.alt' }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/happy_hour">
+                                            Happy Hour
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/contact">
+                                <MenuItem
+                                    sx={{ bgcolor: 'primary.main' }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/contact">
+                                            Contact
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
+                            <StyledMenuLink to="/about">
+                                <MenuItem
+                                    sx={{ bgcolor: 'primary.alt' }}
+                                    onClick={popupState.close}
+                                >
+                                    <Typography>
+                                        <StyledMenuLink to="/about">
+                                            About
+                                        </StyledMenuLink>
+                                    </Typography>
+                                </MenuItem>
+                            </StyledMenuLink>
                         </Menu>
                     </>
                 )}
